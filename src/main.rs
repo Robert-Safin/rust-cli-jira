e9dfd645f5065cad5ec1c4548115a4b8a2217087
Robert Safin <robertsafin98@gmail.com>
2024-02-17 17:14:30 +0800

    re

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,55 @@
+use std::rc::Rc;
+
 mod models;
+
 mod db;
+use db::*;
 
+mod ui;
 
-use std::fs;
+mod io_utils;
+use io_utils::*;
 
-
-use crate::models::{DBState, Epic, Status, Story};
-
+mod navigator;
+use navigator::*;
 
 fn main() {
-    let file = fs::read_to_string("./db/db.json").unwrap();
-    let parsed: DBState = serde_json::from_str(&file).unwrap();
-    println!("{parsed:?}")
+    let db = Rc::new(JiraDatabase::new("./data/db.json".to_owned()));
+    let mut navigator = Navigator::new(Rc::clone(&db));
+
+    loop {
+        clearscreen::clear().unwrap();
+
+        if let Some(page) = navigator.get_current_page() {
+            if let Err(error) = page.draw_page() {
+                println!(
+                    "Error rendering page: {}\nPress any key to continue...",
+                    error
+                );
+                wait_for_key_press();
+            };
+
+            let user_input = get_user_input();
+
+            match page.handle_input(user_input.trim()) {
+                Err(error) => {
+                    println!(
+                        "Error getting user input: {}\nPress any key to continue...",
+                        error
+                    );
+                    wait_for_key_press();
+                }
+                Ok(action) => {
+                    if let Some(action) = action {
+                        if let Err(error) = navigator.handle_action(action) {
+                            println!("Error handling processing user input: {}\nPress any key to continue...", error);
+                            wait_for_key_press();
+                        }
+                    }
+                }
+            }
+        } else {
+            break;
+        }
+    }
 }
